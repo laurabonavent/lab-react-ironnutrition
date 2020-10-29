6ea41db0a270ce27817c4d9cc2d37988c261715f
Sarah Damag <sarahdamag@gmail.com>
2020-10-29 20:21:03 +0100

    iteration 4

--- src/App.js
+++ src/App.js
@@ -2,21 +2,10 @@ import React from 'react';
 import './App.css';
 import FoodList from './components/FoodList';
 
-
 function App() {
-  // const food = {
-  //   image: 'https://i.imgur.com/eTmWoAN.png',
-  //   name: 'Pizza',
-  //   calories: '400',
-  //   quantity: '1',
-  // };
-
-  
-
   return (
     <div className="App">
       <FoodList />
-    
     </div>
   );
 }
--- src/components/FoodList.js
+++ src/components/FoodList.js
@@ -6,38 +6,67 @@ import AddFood from './AddFood';
 class FoodList extends React.Component {
   state = {
     foods: foods,
-    active:false
+    active: false,
+    searchContent: '',
   };
 
   addFoodHandler = (food) => {
     this.setState({
-      foods: [...this.state.foods, food]
+      foods: [...this.state.foods, food],
     });
   };
 
   changeActiveState = () => {
     this.setState({
-      active:false
-    })
-  }
+      active: false,
+    });
+  };
+
+  searchFood = (event) => {
+    this.setState({
+      searchContent: event.target.value,
+    });
+  };
 
   render() {
+    // filtrer search
+    let foods = this.state.foods;
+    let searchContent = this.state.searchContent;
+    if (searchContent) {
+      foods = foods.filter((el) => el.name.includes(searchContent));
+    }
     return (
       <div>
+        {/* Searchbar */}
+        <input
+          type="text"
+          name="search"
+          value={this.state.SearchContent}
+          onChange={this.searchFood}
+        />
+        {/* liste de foods */}
         <ul>
-          {this.state.foods.map((food, index) => (
+          {foods.map((food, index) => (
             <li key={index}>
               <FoodBox {...food} />
             </li>
           ))}
         </ul>
-        <button onClick={(event)=>
-          this.setState({
-            active:true
-          })
-        }>ADD FOOD</button>
-        {this.state.active && <AddFood newFood={this.addFoodHandler} changeState={this.changeActiveState}/> }
-        
+        <button
+          onClick={(event) =>
+            this.setState({
+              active: true,
+            })
+          }
+        >
+          ADD FOOD
+        </button>
+        {this.state.active && (
+          <AddFood
+            newFood={this.addFoodHandler}
+            changeState={this.changeActiveState}
+          />
+        )}
       </div>
     );
   }
